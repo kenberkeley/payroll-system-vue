5fe62b503a68f658f58d2013d3dea0e3cbc270d4
kenberkeley <kenberkeley@foxmail.com>
2019-06-12 20:17:21 +1000

    Correct devServer.proxy config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const { API_PORT } = require('./api/config')
 
 module.exports = {
   devServer: {
-    proxy: `http://localhost:${API_PORT}`
+    proxy: {
+      '/api': {
+        target: `http://localhost:${API_PORT}`,
+        pathRewrite: { '^/api': '' }
+      }
+    }
   }
 }
