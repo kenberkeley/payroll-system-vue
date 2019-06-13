8410bd6f03cfa40a726bffb4b9928bfe0fea294d
kenberkeley <kenberkeley@foxmail.com>
2019-06-13 20:22:45 +1000

    Use vuelidate as the form validation tool

--- src/pages/generator/1-capture.vue
+++ src/pages/generator/1-capture.vue
@@ -9,16 +9,32 @@
         <div class="field">
           <label class="label">First Name</label>
           <div class="control">
-            <input type="text" v-model.trim.lazy="firstName" class="input" required>
+            <input
+              type="text"
+              v-model.trim.lazy="firstName"
+              :class="['input', { 'is-danger': $v.firstName.$error }]"
+              required
+            >
           </div>
+          <p v-if="$v.firstName.$error" class="help is-danger">
+            First name is required
+          </p>
         </div>
       </div>
       <div class="column is-half">
         <div class="field">
           <label class="label">Last Name</label>
           <div class="control">
-            <input type="text" v-model.trim.lazy="lastName" class="input" required>
+            <input
+              type="text"
+              v-model.trim.lazy="lastName"
+              :class="['input', { 'is-danger': $v.lastName.$error }]"
+              required
+            >
           </div>
+          <p v-if="$v.lastName.$error" class="help is-danger">
+            Last name is required
+          </p>
         </div>
       </div>
       <div class="column is-half">
@@ -26,9 +42,18 @@
           <label class="label">Annual Salary</label>
           <div class="control has-icons-left">
             <span class="icon is-left">$</span>
-            <input type="number" step="0.01" min="0"
-              v-model.lazy="annualIncome" class="input" required>
+            <input
+              type="number"
+              step="0.01"
+              min="0"
+              v-model.lazy="annualIncome"
+              :class="['input', { 'is-danger': $v.annualIncome.$error }]"
+              required
+            >
           </div>
+          <p v-if="$v.annualIncome.$error" class="help is-danger">
+            Annual salary should be ≥ 0
+          </p>
         </div>
       </div>
       <div class="column is-half">
@@ -36,9 +61,19 @@
           <label class="label">Superannuation Rate</label>
           <div class="control has-icons-left">
             <span class="icon is-left">%</span>
-            <input type="number" step="0.01" min="0" max="100"
-              v-model.lazy="superRate" class="input" required>
+            <input
+              type="number"
+              step="0.01"
+              min="0"
+              max="100"
+              v-model.lazy="superRate"
+              :class="['input', { 'is-danger': $v.superRate.$error }]"
+              required
+            >
           </div>
+          <p v-if="$v.superRate.$error" class="help is-danger">
+            Superannuation rate should be between [0, 100]
+          </p>
         </div>
       </div>
       <div class="column">
@@ -57,9 +92,11 @@
 import { mapFields } from 'vuex-map-fields'
 import Headerr from './_comps/Header.vue'
 import roundNum from './_utils/roundNum'
+import validate from './_mixins/validate'
 import module from './_store'
 
 export default {
+  mixins: [validate],
   components: { Headerr },
   metaInfo: {
     title: 'Capture Employee Info'
@@ -76,7 +113,10 @@ export default {
   })(Object.keys(module.state)),
   methods: {
     submit () {
-      this.$router.push('/generator/preview')
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        this.$router.push('/generator/preview')
+      }
     }
   }
 }
---
+++ src/pages/generator/_mixins/validate.js
@@ -0,0 +1,24 @@
+import { validationMixin } from 'vuelidate'
+import { required, decimal, minValue, between } from 'vuelidate/lib/validators'
+
+export default {
+  mixins: [validationMixin],
+  validations: {
+    firstName: {
+      required
+    },
+    lastName: {
+      required
+    },
+    annualIncome: {
+      required,
+      decimal,
+      minValue: minValue(0)
+    },
+    superRate: {
+      required,
+      decimal,
+      between: between(0, 100)
+    }
+  }
+}
