cecf8ac29c72658f0bd650dd71c58c52a8fd1691
kenberkeley <kenberkeley@foxmail.com>
2019-06-13 11:59:38 +1000

    Add notice about JWT in localStorage

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,6 +4,13 @@ const LS_KEY = '__TOKEN__'
 // Avoid frequent I/O by using a closure variable
 let token = LS.get(LS_KEY)
 
+/**
+  Notice: the token in localStorage could be maliciously altered
+  The only way to authenticate is making a request with it (e.g. GET /whoami)
+  If the response status code is:
+    - 200: valid
+    - 401: invalid
+ */
 export default {
   get () {
     return token
